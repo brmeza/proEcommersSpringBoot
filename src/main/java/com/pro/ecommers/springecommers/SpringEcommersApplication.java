b9d3048de5620527246549ec688de0f83a755e21
brayan <brayanmesz1998@gmail.com>
2024-04-11 18:42:45 -0500

    Estructurar del proyecto y creacion de las clases para el modelo

---
+++ src/main/java/com/pro/ecommers/springecommers/SpringEcommersApplication.java
@@ -0,0 +1,13 @@
+package com.pro.ecommers.springecommers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommersApplication.class, args);
+	}
+
+}
