5c032313b1ce9baa0874617c05e0f32e158010bb
brayan <brayanmesz1998@gmail.com>
2024-04-12 18:41:11 -0500

    funcionalidad mostrar productos para el admin

--- src/main/java/com/pro/ecommers/springecommers/model/Producto.java
+++ src/main/java/com/pro/ecommers/springecommers/model/Producto.java
@@ -55,14 +55,6 @@ public class Producto {
         this.descripcion = descripcion;
     }
 
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
     public String getImagen() {
         return imagen;
     }
@@ -71,6 +63,17 @@ public class Producto {
         this.imagen = imagen;
     }
 
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
--- src/main/java/com/pro/ecommers/springecommers/service/ProductoService.java
+++ src/main/java/com/pro/ecommers/springecommers/service/ProductoService.java
@@ -2,15 +2,17 @@ package com.pro.ecommers.springecommers.service;
 
 import com.pro.ecommers.springecommers.model.Producto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductoService {
     //definir metodos CRUD para la table producto;
 
     public Producto Save (Producto producto);
-    public Optional<Producto> get(Integer id);
-    //opcional nos la opcion si el objeto que llamamos de la base existe
-
+    public Optional<Producto> get(Integer id);//opcional nos la opcion si el objeto que llamamos de la base existe
     public void update (Producto producto);
     public  void delete(Integer id);
+
+    public List<Producto> findAll();
+
 }
