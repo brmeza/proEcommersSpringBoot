ab9800e4c6468ab0766eff54128aca515167727a
brayan <brayanmesz1998@gmail.com>
2024-04-15 16:20:15 -0500

    validacion para doble producto, ver el carrito desde la cabecera del template usuario

--- src/main/java/com/pro/ecommers/springecommers/controller/HomeController.java
+++ src/main/java/com/pro/ecommers/springecommers/controller/HomeController.java
@@ -45,7 +45,7 @@ public class HomeController {
         return "usuario/productohome";
     }
 
-    @PostMapping("cart")
+    @PostMapping("/cart")
     public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model){
         DetalleOrden detalleOrden = new DetalleOrden();
         Producto producto = new Producto();
@@ -61,7 +61,12 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio()*cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar que el producto no se añada dos veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
+        if(!ingresado){
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
         orden.setTotal(sumaTotal);
@@ -88,6 +93,13 @@ public class HomeController {
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
         return "usuario/carrito";
+    }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+        return "/usuario/carrito";
     }
 
 }
