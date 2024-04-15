7f6acd2147748f6c9b60f89f5bc9245d8bbd5cb5
brayan <brayanmesz1998@gmail.com>
2024-04-15 12:20:03 -0500

    Funcionalidad mostrar Img en la home del usuario, HomeController y home

---
+++ src/main/java/com/pro/ecommers/springecommers/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.pro.ecommers.springecommers.controller;
+
+import com.pro.ecommers.springecommers.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("")
+    public String Home(Model model){
+        model.addAttribute("productos",productoService.findAll());
+        return "usuario/home";
+    }
+}
