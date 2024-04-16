69046dba7a8e3bef70b8f174e7f21f87819efec7
brayan <brayanmesz1998@gmail.com>
2024-04-16 16:50:35 -0500

    metodo parta generar el numero de la orden

--- src/main/java/com/pro/ecommers/springecommers/service/IOrdenService.java
+++ src/main/java/com/pro/ecommers/springecommers/service/IOrdenService.java
@@ -2,7 +2,10 @@ package com.pro.ecommers.springecommers.service;
 
 import com.pro.ecommers.springecommers.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
     public Orden save(Orden orden);
+    public List<Orden>findAll();
 
 }
