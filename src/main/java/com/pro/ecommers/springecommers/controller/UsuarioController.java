9d626b14b84a84205538d19068444ade3e8100e5
brayan <brayanmesz1998@gmail.com>
2024-04-17 14:39:45 -0500

    mostrar la vista compras para el user

--- src/main/java/com/pro/ecommers/springecommers/controller/UsuarioController.java
+++ src/main/java/com/pro/ecommers/springecommers/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,8 @@ public class UsuarioController {
 
     @Autowired
     private IUsuarioServices usuarioService;
+    @Autowired
+    private HttpSession httpSession;
 
     @GetMapping("/registro")
     public String create(){
@@ -57,6 +60,14 @@ public class UsuarioController {
             log.info("Usuario no existe");
         }
         return "redirect:/";
+    }
+
+
+    @GetMapping("/compras")
+    public String obtenerCompras(Model model, HttpSession sesion){
+        model.addAttribute("sesion", sesion.getAttribute("idUsuario"));
+        return "usuario/compras";
+
     }
 
 }
