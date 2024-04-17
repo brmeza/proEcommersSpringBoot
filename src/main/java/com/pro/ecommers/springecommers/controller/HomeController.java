93632710b880eca7da385b2dad892d677ff00aba
brayan <brayanmesz1998@gmail.com>
2024-04-16 19:14:19 -0500

    funcionalidad buscar un producto

--- src/main/java/com/pro/ecommers/springecommers/controller/HomeController.java
+++ src/main/java/com/pro/ecommers/springecommers/controller/HomeController.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -149,6 +150,15 @@ public class HomeController {
         detalles.clear();
 
         return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String searchProduct(@RequestParam String nombre, Model model){
+        log.info("nombre del producto {}",nombre);
+        List<Producto> productos = productoService.findAll().stream().filter(p -> p.getNombre().contains(nombre)).collect(Collectors.toList());
+        model.addAttribute("productos", productos);
+
+        return "usuario/home";
     }
 
 
