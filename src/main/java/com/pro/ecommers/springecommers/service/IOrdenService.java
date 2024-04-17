bade3dd52aaae779711a375eeaf82da24959de96
brayan <brayanmesz1998@gmail.com>
2024-04-17 18:15:28 -0500

    funcionalidad ver detalle compra al user

--- src/main/java/com/pro/ecommers/springecommers/service/IOrdenService.java
+++ src/main/java/com/pro/ecommers/springecommers/service/IOrdenService.java
@@ -4,11 +4,13 @@ import com.pro.ecommers.springecommers.model.Orden;
 import com.pro.ecommers.springecommers.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrdenService {
     public Orden save(Orden orden);
     public List<Orden>findAll();
     String gerararNumeroOrden();
     List<Orden>findByUsuario(Usuario usuario);
+    Optional<Orden> findById (Integer id);
 
 }
--- src/main/java/com/pro/ecommers/springecommers/service/OrdenSericeImp.java
+++ src/main/java/com/pro/ecommers/springecommers/service/OrdenSericeImp.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrdenSericeImp implements IOrdenService{
@@ -56,6 +57,11 @@ public class OrdenSericeImp implements IOrdenService{
     @Override
     public List<Orden> findByUsuario(Usuario usuario) {
         return ordenRepository.findByUsuario(usuario);
+    }
+
+    @Override
+    public Optional<Orden> findById(Integer id) {
+        return ordenRepository.findById(id);
     }
 
 }
